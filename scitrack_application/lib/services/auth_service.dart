e1a8defc63f288c26bf0ee8f92bcbe1aeb70f507
Samirborges <alessandro.samir@souunit.com.br>
2026-05-27 11:57:37 -0300

    feat(auth_service): add method getUserData and updateProfile

--- scitrack_application/lib/services/auth_service.dart
+++ scitrack_application/lib/services/auth_service.dart
@@ -1,37 +1,63 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-
   static Future<void> registerUser({
+    required String name,
+    required String institution,
+    required String researchArea,
     required String email,
     required String password,
   }) async {
+    final prefs = await SharedPreferences.getInstance();
 
-    final prefs =
-        await SharedPreferences.getInstance();
-
+    await prefs.setString('name', name);
+    await prefs.setString('institution', institution);
+    await prefs.setString('researchArea', researchArea);
     await prefs.setString('email', email);
     await prefs.setString('password', password);
   }
-
 
   static Future<bool> login({
     required String email,
     required String password,
   }) async {
+    final prefs = await SharedPreferences.getInstance();
 
-    final prefs =
-        await SharedPreferences.getInstance();
+    final savedEmail = prefs.getString('email');
 
-    final savedEmail =
-        prefs.getString('email');
+    final savedPassword = prefs.getString('password');
 
-    final savedPassword =
-        prefs.getString('password');
+    return (email == savedEmail && password == savedPassword);
+  }
 
-    return (
-      email == savedEmail &&
-      password == savedPassword
-    );
+  static Future<Map<String, String>> getUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return {
+      'name': prefs.getString('name') ?? '',
+
+      'institution': prefs.getString('institution') ?? '',
+
+      'researchArea': prefs.getString('researchArea') ?? '',
+
+      'email': prefs.getString('email') ?? '',
+    };
+  }
+
+  static Future<void> updateProfile({
+    required String name,
+    required String institution,
+    required String researchArea,
+    required String email,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('name', name);
+
+    await prefs.setString('institution', institution);
+
+    await prefs.setString('researchArea', researchArea);
+
+    await prefs.setString('email', email);
   }
 }
